5b1d1dfd23138686fb2831179190bff4e6cb48b4
therealmbittarelli <maggiekmcclellan@gmail.com>
2019-12-02 15:42:13 -0500

    first drill complete

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const app = express();
 app.use(morgan('dev'));
 
 //This is the final request handler
-app.get('/', (req, res) => {
-  res.send('Hello Express!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello Express!');
+// });
 
-app.listen(8000, () => {
-  console.log('Express server is listening on port 8000!');
+app.listen(8001, () => {
+  console.log('Express server is listening on port 8001!');
 });
 
 app.get('/sum', (req, res) => {
@@ -21,12 +21,30 @@ app.get('/sum', (req, res) => {
 
   if (!a) {
     return res.status(400).send('A is required.');
-
   }
 
   if (!b) {
     return res.status(400).send('B is required.');
   }
+
+  const numberA = parseInt(a);
+  const numberB = parseInt(b);
+
+  if (Number.isNaN(numberA)) {
+    return 'NumberA is NaN';
+  }
+
+  if (Number.isNaN(numberB)) {
+    return res.status(400).send('NumberB is not a number.');
+  }
+
+
+  const c = numberA + numberB;
+
+
+  const sumResponse = `The sum of ${numberA} and ${numberB} is ${c}.`;
+
+  res.send(sumResponse);
 
 
 
